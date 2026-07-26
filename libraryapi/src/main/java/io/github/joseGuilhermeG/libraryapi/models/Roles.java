47f04e99833e752ebe065b6d70f31f03abff98ce
Guilherme <jg21504956@gmail.com>
2026-07-26 19:20:26 -0300

    criando o model de user e role

--- libraryapi/src/main/java/io/github/joseGuilhermeG/libraryapi/models/Roles.java
+++ libraryapi/src/main/java/io/github/joseGuilhermeG/libraryapi/models/Roles.java
@@ -1,4 +1,28 @@
 package io.github.joseGuilhermeG.libraryapi.models;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table
+@Getter
+@Setter
+@ToString
 public class Roles {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer id;
+
+    @Column(length = 50 , unique = true , nullable = false)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users = new HashSet<>();
+
 }
--- libraryapi/src/main/java/io/github/joseGuilhermeG/libraryapi/models/User.java
+++ libraryapi/src/main/java/io/github/joseGuilhermeG/libraryapi/models/User.java
@@ -1,4 +1,36 @@
 package io.github.joseGuilhermeG.libraryapi.models;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@ToString
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true , nullable = false)
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @ManyToMany(cascade = CascadeType.ALL , fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private List<Roles> roles;
+
 }
